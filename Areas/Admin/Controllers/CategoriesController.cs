41e60467c631e9322dafa9ba9026f790affd4ebc
havuh <havuh@DESKTOP-FGGMQPK>
2025-10-25 21:54:55 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyStore.Models;
+
+namespace MyStore.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private MyStoreEntities db = new MyStoreEntities();
+
+        // GET: Admin/Categories : lấy dữ liệu từ bảng Categories và hiển thị ra view Index
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Admin/Categories/Details/5
+        public ActionResult Details(int? id) //Details lấy chi tiết 1 bản ghi có CategoryID = giá trị id truyền vào
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); //Mã lỗi 404: lỗi nếu thiếu giá trị chuyền vào
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null) //lỗi nếu không tìm thấy bản ghi
+            {
+                return HttpNotFound(); //Mã lỗi 404
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Create
+        //[HttpGet] là phương thức mặc định nên không cần khai báo từ khóa
+        public ActionResult Create() //Load form Create
+        {
+            return View();
+        }
+
+        // POST: Admin/Categories/Create
+        //POST: Lưu dữ liệu nhập vào từ form create vào database
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost] //phải khai báo phương thức POST, nếu không sẽ lỗi tạo thiếu phương thức
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
